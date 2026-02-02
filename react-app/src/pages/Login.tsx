9d74fc378cfcbd2df03520847c23c0955546cc52
Mikko Sio <113332482+mikkosio@users.noreply.github.com>
2026-02-01 21:54:09 -0800

    feat(pages): add placeholder pages

---
+++ react-app/src/pages/Login.tsx
@@ -0,0 +1,10 @@
+const LoginPage = () => {
+    return (
+        <div>
+            <h1>Login Page</h1>
+            <p>🚧 Work in progress</p>
+        </div>
+    )
+}
+
+export default LoginPage;
