6cdef48c41cbbfc4f139892939d62edb8e9a8c8c
Amrit Jhatu <a_jhatu@hotmail.com>
2026-02-02 18:01:40 -0800

    Skeleton of sign up sheet made.

--- react-app/src/pages/SignUp.tsx
+++ react-app/src/pages/SignUp.tsx
@@ -1,8 +1,24 @@
 const SignUpPage = () => {
     return (
         <div>
-            <h1>SignUp Page</h1>
-            <p>🚧 Work in progress</p>
+            <h1>Sign Up</h1>
+            <form>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" />
+                </div>
+                
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" />
+                </div>
+                <div>
+                    <label htmlFor="confirmpassword">Confirm Password:</label>
+                    <input type="password" id="confirmpassword" />
+                </div>
+                
+                <button type="submit">Sign Up</button>
+            </form>
         </div>
     )
 }
