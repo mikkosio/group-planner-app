7eb8f2ce9e7e4ba493bc4a5fae728de64929f471
Mikko Sio <113332482+mikkosio@users.noreply.github.com>
2026-03-06 00:35:58 -0800

    feat(navbar): show navbar in auth routes

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -5,34 +5,33 @@ import Login from './pages/login/Login'
 import SignUp from './pages/signup/SignUp'
 import Profile from './pages/Profile'
 import Landing from './pages/Landing'
-import Navbar from './components/Navbar/Navbar'
 import { AuthProvider } from './providers/AuthProvider'
 import ProtectedRoute from './components/ProtectedRoute'
 import GuestRoute from './components/GuestRoute'
+import Layout from './components/Layout'
 
 function App() {
     return (
         <AuthProvider>
             <Router>
-                {/* Navbar */}
-                <Navbar></Navbar>
-
-                {/* Routes */}
-                <Routes>
-                    <Route path="/" element={<Landing />} />
-                    <Route path="/login" element={
-                        <GuestRoute>
-                            <Login />
-                        </GuestRoute>
-                    }/>
-                    <Route path="/signup" element={
-                        <GuestRoute>
-                            <SignUp />
-                        </GuestRoute>
-                    } />
-                    <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-                    <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-                </Routes>
+                <Layout>
+                    {/* Routes */}
+                    <Routes>
+                        <Route path="/" element={<Landing />} />
+                        <Route path="/login" element={
+                            <GuestRoute>
+                                <Login />
+                            </GuestRoute>
+                        }/>
+                        <Route path="/signup" element={
+                            <GuestRoute>
+                                <SignUp />
+                            </GuestRoute>
+                        } />
+                        <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+                        <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+                    </Routes>
+                </Layout>
             </Router>
         </AuthProvider>
     )
