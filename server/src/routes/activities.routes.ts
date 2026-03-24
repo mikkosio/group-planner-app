24e4dd26d05984a63d2acbefffc0ba5d05babf9f
Mikko Sio <113332482+mikkosio@users.noreply.github.com>
2026-03-23 22:44:47 -0700

    chore(activities): remove unnecessary import

--- server/src/routes/activities.routes.ts
+++ server/src/routes/activities.routes.ts
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import { protect } from "../middlewares/authMiddleware";
 import { validateRequest } from "../middlewares/validateRequest";
 import {
     createActivitySchema,
