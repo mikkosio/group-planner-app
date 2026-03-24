9889d085feb2bea06e86a00ded53303ff8730079
Mikko Sio <113332482+mikkosio@users.noreply.github.com>
2026-03-23 23:08:06 -0700

    fix(invite): add info theme to fix missing info snackbar color

--- react-app/src/theme.ts
+++ react-app/src/theme.ts
@@ -1,5 +1,5 @@
 import { createTheme } from "@mui/material/styles";
-import { lightBlue, cyan, amber, red } from "@mui/material/colors";
+import { lightBlue, cyan, amber, red, blue } from "@mui/material/colors";
 
 const theme = createTheme({
     palette: {
@@ -14,6 +14,12 @@ const theme = createTheme({
             main: cyan.A400,
             dark: cyan.A700,
             contrastText: lightBlue[900],
+        },
+        info: {
+            light: blue[100],
+            main: blue[500],
+            dark: blue[900],
+            contrastText: "#fff",
         },
         error: {
             light: red[50],
